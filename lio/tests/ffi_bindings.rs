a8892b95aeca0c9d67704ce72fa6100de41a8ccc
Vincent Thomas <vincent@v-thomas.com>
2025-12-02 21:23:07 +0100

    very nice

--- lio/tests/ffi_bindings.rs
+++ lio/tests/ffi_bindings.rs
@@ -90,6 +90,8 @@ fn test_ffi_bindings() {
   // Verify library exists
   let lib_path = target_dir().join(&format!("liblio.{EXT}"));
   assert!(lib_path.exists(), "liblio.{} was not built", EXT);
+  dbg!(std::fs::read_dir(target_dir()).unwrap().collect::<Vec<_>>());
+  panic!();
 
   // Create a simple C test file
   let c_source = target_dir().join("test_ffi_compile.c");
