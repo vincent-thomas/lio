314181ed04b81be7d49169df8ef0f6757951b095
Vincent Thomas <vincent@v-thomas.com>
2025-12-02 22:26:52 +0100

    very nice

--- lio/tests/ffi/test.sh
+++ lio/tests/ffi/test.sh
@@ -12,8 +12,9 @@ make lio-cbuild
 echo "lio C compilation successful"
 
 # Compile C test
-gcc  "$SCRIPT_DIR/c-src.c" \
+gcc "$SCRIPT_DIR/c-src.c" \
     -L"$TARGET_DIR" \
+    -Wl,-rpath,"$TARGET_DIR" \
     -o "$TARGET_DIR/test_ffi_c" \
     -llio \
     -I"$PROJECT_ROOT/lio/include"
@@ -34,13 +35,14 @@ echo "C test passed"
 # Compile C++ test
 g++ "$SCRIPT_DIR/cpp-src.cpp" \
     -L"$TARGET_DIR" \
+    -Wl,-rpath,"$TARGET_DIR" \
     -o "$TARGET_DIR/test_ffi_cpp" \
     -llio \
     -I"$PROJECT_ROOT/lio/include"
 
 echo "C++ compilation successful"
 
-# # Run C++ test
+# Run C++ test
 $TARGET_DIR/test_ffi_cpp
 
 echo "C++ test passed"
